41aa6c44a44a71ca83fae125c34b960ee78a9b73
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-01-27 12:35:57 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React from "react";
-import Home from "./components/Home";
-import Counter from "./components/Counter";
-import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { BrowserRouter as Router, Link, useRoutes } from "react-router-dom";
+import Home from "./components/Home";
+import Counter from "./components/Counter";
+import Drag from "./components/Drag";
+
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    margin: 10,
+  },
+  link: {
+    color: "inherit",
+    textDecoration: "none",
+  },
+}));
 
 const App = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "counter", element: <Counter /> },
+    { path: "drag", element: <Drag /> },
   ]);
   return routes;
 };
 
 const AppWrapper = () => {
+  const classes = useStyles();
   return (
     <Router>
-      <Button variant="contained">
-        <Link to="/">Home</Link>
-      </Button>
-      <Button variant="contained">
-        <Link to="/counter">Counter</Link>
-      </Button>
-      <App />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.root}>
+            <Link to="/" className={classes.link}>
+              RxJS
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <App />
+        </Grid>
+      </Container>
     </Router>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    margin: 10,
+  },
+}));
 
 function Home() {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Grid item xs={12}>
+        <h1>Examples</h1>
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          href="/counter"
+          variant="contained"
+          color="primary"
+          className={classes.root}
+        >
+          Counter
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          href="/drag"
+          variant="contained"
+          color="primary"
+          className={classes.root}
+        >
+          Drag
+        </Button>
+      </Grid>
+    </>
   );
 }
 
